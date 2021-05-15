6872be1aeb5222cc5b9e9a7092edbb0d65c2d1f5
MHammadYou <MHammadY2.0@gmail.com>
2021-05-15 23:03:34 +0500

    Added PrizeModel

---
+++ TrackerLibrary/PrizeModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TrackerLibrary
+{
+    public class PrizeModel
+    {
+        public int PlaceNumber { get; set; }
+        public string PlaceName { get; set; }
+        public decimal PriceAmount { get; set; }
+        public double PrizePercentage { get; set; }
+    }
+}

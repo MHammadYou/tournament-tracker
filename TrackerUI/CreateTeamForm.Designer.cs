0a0ba3f90432b6f0a3ac1f1e557979df82ee1500
MHammadYou <MHammadY2.0@gmail.com>
2021-06-02 23:35:33 +0500

    Added Create Form Component

--- TrackerUI/CreateTeamForm.Designer.cs
+++ TrackerUI/CreateTeamForm.Designer.cs
@@ -31,20 +31,59 @@ namespace TrackerUI
         /// </summary>
         private void InitializeComponent()
         {
+            this.tournamentNameValue = new System.Windows.Forms.TextBox();
+            this.teamNameLabel = new System.Windows.Forms.Label();
+            this.TeamLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // tournamentNameValue
+            // 
+            this.tournamentNameValue.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.tournamentNameValue.Location = new System.Drawing.Point(32, 114);
+            this.tournamentNameValue.Name = "tournamentNameValue";
+            this.tournamentNameValue.Size = new System.Drawing.Size(230, 29);
+            this.tournamentNameValue.TabIndex = 13;
+            // 
+            // teamNameLabel
+            // 
+            this.teamNameLabel.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.teamNameLabel.ForeColor = System.Drawing.Color.FromArgb(((int) (((byte) (51)))), ((int) (((byte) (152)))), ((int) (((byte) (255)))));
+            this.teamNameLabel.Location = new System.Drawing.Point(24, 85);
+            this.teamNameLabel.Name = "teamNameLabel";
+            this.teamNameLabel.Size = new System.Drawing.Size(170, 26);
+            this.teamNameLabel.TabIndex = 12;
+            this.teamNameLabel.Text = "Team Name";
+            // 
+            // TeamLabel
+            // 
+            this.TeamLabel.ForeColor = System.Drawing.Color.FromArgb(((int) (((byte) (51)))), ((int) (((byte) (153)))), ((int) (((byte) (255)))));
+            this.TeamLabel.Location = new System.Drawing.Point(24, 22);
+            this.TeamLabel.Name = "TeamLabel";
+            this.TeamLabel.Size = new System.Drawing.Size(191, 30);
+            this.TeamLabel.TabIndex = 11;
+            this.TeamLabel.Text = "Create Team";
             // 
             // CreateTeamForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1370, 749);
+            this.ClientSize = new System.Drawing.Size(784, 539);
+            this.Controls.Add(this.tournamentNameValue);
+            this.Controls.Add(this.teamNameLabel);
+            this.Controls.Add(this.TeamLabel);
             this.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
             this.Name = "CreateTeamForm";
             this.Text = "Create Team";
             this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
+        private System.Windows.Forms.Label TeamLabel;
+        private System.Windows.Forms.Label teamNameLabel;
+
+        private System.Windows.Forms.TextBox tournamentNameValue;
 
         #endregion
     }

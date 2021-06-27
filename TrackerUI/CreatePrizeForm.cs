33e5210c69cbf5725b59ba1bf226a1a03581937d
MHammadYou <MHammadY2.0@gmail.com>
2021-06-27 23:38:44 +0500

    Added pzize creation

--- TrackerUI/CreatePrizeForm.cs
+++ TrackerUI/CreatePrizeForm.cs
@@ -21,6 +21,11 @@ namespace TrackerUI
                     prizeAmountValue.Text,
                     prizePercentageValue.Text
                     );
+                
+                foreach (IDataConnection db in GlobalConfig.Connections)
+                {
+                    db.CreatePrize(model);
+                }
             }
         }
 
